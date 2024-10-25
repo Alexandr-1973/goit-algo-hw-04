e9bf105453ba1d745a50edbe0f47b98471bfe21a
Alexandr-1973 <144156771+Alexandr-1973@users.noreply.github.com>
2024-10-25 23:09:32 +0300

    change-task1-and-task3

--- task3.py
+++ task3.py
@@ -1,23 +1,30 @@
-from colorama import Fore
 import sys
 from pathlib import Path
+from colorama import Fore
 
-def iter_directory_colours(directory):
-    for path in directory:
-        if path.is_dir():
-            print(f"{Fore.BLUE} {path} {Fore.RESET}")
-            iter_directory_colours(Path(path).iterdir())
+def iter_directory_colours(directory, start_stick=""):
+    directory_list = list(directory)
+    for index, path in enumerate(directory_list):
+        name_item = Path(path)
+        if index == len(directory_list) - 1:
+            last_stick = "┗ "
+            new_start_stick = start_stick + "  "
         else:
-            print(f"{Fore.GREEN} {path} {Fore.RESET}")
-
+            last_stick = "┣ "
+            new_start_stick = start_stick + "┃ "
+        if name_item.is_dir():
+            print(f"{start_stick}{last_stick}📂{Fore.BLUE}{name_item.name}{Fore.RESET}")
+            iter_directory_colours(name_item.iterdir(), new_start_stick)
+        else:
+            print(f"{start_stick}{last_stick}📜{Fore.GREEN}{name_item.name}{Fore.RESET}")
 
 def main():
     if sys.argv[1] and Path(sys.argv[1]).is_dir():
+        print(f"📦{Fore.YELLOW}{sys.argv[1]}{Fore.RESET}")
         test_dir = Path(sys.argv[1]).iterdir()
         iter_directory_colours(test_dir)
     else:
         print("No directory")
         return
-
 
 main()
